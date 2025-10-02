7ea24b1463d464e494753116cece32318aba6729
RizafiDev <firmansyah.riza6@smk.belajar.id>
2025-10-02 14:55:00 +0700

    responsive hero mobile done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -46,27 +46,27 @@ export default function Hero() {
       <div className="absolute inset-0 bg-black opacity-50 pointer-events-none z-10" />
 
       {/* Content */}
-      <div className="w-full relative flex flex-col space-y-4 text-center mx-auto px-4 sm:px-12 lg:px-26 container items-center justify-center pt-42 pb-46">
-        <h1 className="font-bold text-5xl leading-14 text-white z-10">
+      <div className="w-full relative flex flex-col space-y-4 text-center mx-auto px-4 sm:px-12 lg:px-26 container items-start lg:items-center justify-center pt-42 pb-46">
+        <h1 className="font-bold text-start lg:text-center text-4xl lg:text-5xl leading-12 lg:leading-14 text-white z-10">
           Buat Twibbon <span className="font-fancy">&</span> Shortlink
-          <br />
-          Semua dalam Sekejap <span className="font-fancy">: )</span>
+          <br className="hidden lg:flex" />
+          {" "}Semua dalam Sekejap <span className="font-fancy">: )</span>
         </h1>
-        <p className="text-white max-w-[600px] text-lg font-medium z-10">
+        <p className="text-white max-w-[600px] text-base text-start lg:text-center lg:text-lg font-medium z-10">
           Tingkatkan jangkauan kampanye, branding, dan promosi hanya dengan
           sekali klik. Semua serba cepat, praktis, dan terintegrasi.
         </p>
         <div className="cta flex items-center gap-4 z-10">
           <Link
             href={"#"}
-            className="flex gap-2 items-center truncate bg-transparent text-white border-2 hover:bg-white hover:border-white border-white text-sm hover:text-foreground font-semibold px-5 py-3 rounded-full hover:bg-secondary duration-200  transition-all"
+            className="flex gap-2 items-center truncate bg-transparent text-white border-2 hover:bg-white hover:border-white border-white text-xs lg:text-sm hover:text-foreground font-semibold px-5 py-3 rounded-full hover:bg-secondary duration-200  transition-all"
           >
             Mulai Sekarang
             <SendHorizonal className="size-4" />
           </Link>
           <Link
             href={"#"}
-            className="flex gap-2 items-center truncate bg-transparent text-white border-2 hover:bg-white hover:border-white border-white text-sm hover:text-foreground font-semibold px-5 py-3 rounded-full hover:bg-secondary duration-200  transition-all"
+            className="flex gap-2 items-center truncate bg-transparent text-white border-2 hover:bg-white hover:border-white border-white text-xs lg:text-sm hover:text-foreground font-semibold px-5 py-3 rounded-full hover:bg-secondary duration-200  transition-all"
           >
             Eksplor Fitur
             <Binoculars className="size-4" />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="w-full fixed top-0 left-0 z-50">
       <div className="w-full py-6 mx-auto px-4 sm:px-12 lg:px-26 container flex  justify-between items-center ">
         <div className="brand flex">
-          <p className="font-bold text-2xl text-white">
+          <p className="font-bold text-xl lg:text-2xl text-white">
             ViskaDigital<span className="align-super text-xs">©</span>
           </p>
         </div>
@@ -14,13 +14,13 @@ export default function Navbar() {
         <div className="cta flex items-center gap-3 ">
           <Link
             href={"#"}
-            className="flex items-center gap-1  duration-200 ease-in-out transition-all  hover:text-primary font-semibold text-xs bg-white px-4 py-3 rounded-full truncate"
+            className="lg:flex items-center gap-1 hidden duration-200 ease-in-out transition-all  hover:text-primary font-semibold text-xs bg-white px-4 py-3 rounded-full truncate"
           >
             <Plus className="size-4 font-bold" /> Buat Link
           </Link>
           <Link
             href={"#"}
-            className="flex items-center gap-1  duration-200 ease-in-out transition-all  hover:text-primary font-semibold text-xs bg-white px-4 py-3 rounded-full truncate"
+            className="lg:flex items-center gap-1 hidden duration-200 ease-in-out transition-all  hover:text-primary font-semibold text-xs bg-white px-4 py-3 rounded-full truncate"
           >
             <UploadCloud className="size-4 font-bold" /> Upload Twibbon
           </Link>
